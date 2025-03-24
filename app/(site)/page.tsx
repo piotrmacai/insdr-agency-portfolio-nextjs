3e96b6bedcce93e01b193497fa085254a98ba763
Piotr Macai <piotrmacai0@gmail.com>
2025-03-24 18:49:11 +0100

    upd front

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
       <MainAbout/>
       {/* <About />    */}
       <FeaturesTab />      
-       <HomePortfolio/>
+       {/* <HomePortfolio/> */}
        {/* <CTAToAinsider/> */}
      
       <CTA />       
