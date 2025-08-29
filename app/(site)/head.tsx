8c212a6117df56ce1ac4b20359d4d561da45ef30
Piotr Macai <piotrmacai0@gmail.com>
2025-08-29 18:50:43 +0200

    upd ogimage3

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -12,14 +12,13 @@ export default function Head() {
 <meta property="og:type" content="website" />
 <meta property="og:title" content="Ainsider AI Company - AI | Web | Automations For Business" />
 <meta property="og:description" content="Tailored To The Needs Of Your Company Web & AI and Automation solutions that help businesses to grow." />
-<meta property="og:image" content="https://ainsider.co/images/logo/ogimage.jpg" />
+{/* <meta property="og:image" content="https://ainsider.co/images/logo/ogimage.jpg" /> */}
+<meta name="og:image" content="https://opengraph.b-cdn.net/production/images/2bebd5f5-5663-4d6a-bf75-0b9e2fd1881b.jpg?token=bxCIjHY0uhOxk34x6CKH_12zn0s4g4-N62m5T9bRtfw&height=615&width=1200&expires=33292485479"/>
+<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/2bebd5f5-5663-4d6a-bf75-0b9e2fd1881b.jpg?token=bxCIjHY0uhOxk34x6CKH_12zn0s4g4-N62m5T9bRtfw&height=615&width=1200&expires=33292485479"/>
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
-
 <meta property="og:logo" content="https://ainsider.co/images/logo/webLogo.png" />
-
 <meta name="twitter:card" content="summary_large_image" />
-<meta name="twitter:image" content="https://ainsider.co/images/logo/ogimage.jpg"/>
     </>
   );
 }
